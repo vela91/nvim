6c44ce6062359af3f0a9557b9f2a223fa1bd269f
Jose Vela <codevela@protonmail.com>
2025-04-16 16:48:14 +0200

    add buloco

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -29,16 +29,30 @@
 -- }
 
 -- Or with configuration
+-- return {
+--   'projekt0n/github-nvim-theme',
+--   name = 'github-theme',
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require('github-theme').setup {
+--       -- ...
+--     }
+--
+--     vim.cmd 'colorscheme github_light'
+--   end,
+-- }
 return {
-  'projekt0n/github-nvim-theme',
-  name = 'github-theme',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('github-theme').setup {
-      -- ...
-    }
-
-    vim.cmd 'colorscheme github_light'
-  end,
+  {
+    'uloco/bluloco.nvim',
+    lazy = false,
+    priority = 1000,
+    dependencies = { 'rktjmp/lush.nvim' },
+    config = function()
+      require('bluloco').setup {
+        style = 'light', -- "auto" | "dark" | "light"
+      }
+      vim.cmd 'colorscheme bluloco'
+    end,
+  },
 }
